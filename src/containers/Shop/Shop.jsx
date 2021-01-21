6f519bf9431f1642df05937eb9dc53290061ddf0
dagreatcode <dagreatcode@gmail.com>
2021-01-21 16:24:02 -0500

    added info to page with jsx {}

--- src/containers/Shop/Shop.jsx
+++ src/containers/Shop/Shop.jsx
@@ -3,16 +3,35 @@ import axios from "axios";
 
 class Shop extends Component {
 
+    state = {
+        products: [],
+    }
+
     componentDidMount(){
         axios.get("https://cors-anywhere.herokuapp.com/https://hub.dummyapis.com/products?noofRecords=10&idStarts=1001&currency=usd").then((response)=>{
             console.log(response.data);
+            this.setState({
+                products: response.data,
+            })
         })
     }
 
   render() {
     return (
-     <div>
-       <h1>Shop Page</h1>
+     <div className="container">
+        <div className="row">
+            <div className="col-sm-2"/>
+            <div className="col-sm-8">
+                {this.state.products.map((product) =>(
+                    <div key={product.id}>
+                        <h1>
+                        {product.name} - {product.price}
+                        </h1>
+                        <p>{product.description}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
      </div>
     );
   }
