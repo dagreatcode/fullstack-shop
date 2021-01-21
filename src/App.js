ed879c0ac91543ecc8a3bcaa02bf8a14f0afe7f4
dagreatcode <dagreatcode@gmail.com>
2021-01-21 09:43:53 -0500

    added nav and style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./containers/Home/Home";
-import Shop from "./containers/Shop/Shop"
+import Shop from "./containers/Shop/Shop";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Router>
+      <Navbar/>
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route exact path="/shop" component={Shop}/>
